583be4c89c58412a203f9fee3adcf513ab02fb5c
Mohamad <mohamad.tammae@gmail.com>
2025-06-08 23:54:30 +0200

    Add files via upload
    test

---
+++ script.js
@@ -0,0 +1,5 @@
+// Beispiel für mobile Menü-Logik
+const nav = document.querySelector('.nav-links');
+const logo = document.querySelector('.logo');
+
+// Erweiterung z.B. Hamburger-Menü hier möglich
